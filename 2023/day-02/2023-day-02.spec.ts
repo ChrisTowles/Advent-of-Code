a5eccd2ddfa24170df416384b344afa8fd095220
Chris Towles <Chris.Towles@gmail.com>
2024-12-02 00:02:21 -0500

    chore: working on 2023 day 2

--- 2023/day-02/2023-day-02.spec.ts
+++ 2023/day-02/2023-day-02.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest'
 import { readFileByLines } from '../../utils/read-file'
-
+import c from 'tinyrainbow'
 
 // Constants
 const YEAR = "2023"
@@ -15,9 +15,17 @@ interface ParsedResult {
   entries: ParsedEntry[];
 }
 
+interface GameSet {
+  red: number,
+  blue: number,
+  green: number
+}
 interface ParsedEntry {
   line: string;
   gameNumber: number
+  sets: GameSet[];
+  included: boolean;
+  
 }
 
 const parseLines = (input: string[]): ParsedResult => {
@@ -26,17 +34,73 @@ const parseLines = (input: string[]): ParsedResult => {
     
 
   for (const line of input) {
-    result.entries.push({ line, gameNumber: parseInt(line.split(' ')[1].replace(':', '')) })
+
+    const gameNumberMatchResult = line.match(/^Game (\d+):/);
+    const entry: ParsedEntry = { 
+      line: line,
+      gameNumber: 0,
+      sets: [],
+      included: true
+    }
+    entry.gameNumber = Number(gameNumberMatchResult![1]);
+
+    let setData = line.split(':')[1];
+    let sets = setData.split(';');
+
+    sets.forEach((set) => {
+      const setMatchResult = set.match(/(\d+) (red|blue|green)/g);
+      if (setMatchResult) {
+        let gameSet: GameSet = { red: 0, blue: 0, green: 0 };
+        setMatchResult.forEach((item) => {
+          const [count, color] = item.split(' ');
+          gameSet[color] = Number(count);
+        });
+
+        entry.sets.push(gameSet);
+      }
+    })
+
+
+    // console.log(gameNumberMatchResult);
+    result.entries.push(entry)
   }
 
   return result;
 }
 
+const printGameSet = (gameSet: GameSet) => {
+  return `r: ${gameSet.red}, b: ${gameSet.blue}, g: ${gameSet.green}`
+}
+
+
 const printResult = (result: ParsedResult) => {
-  const padLength = 15
-  for (let i = 0; i < result.entries.length; i++) {
-    console.log(`${i + 1} - line: ${result.entries[i].line.padEnd(padLength, ' ')}`);
+
+  const printList: any[] = [];
+
+  for (const entry of result.entries) {
+    const printObj = {}
+    printObj['gameNumber'] = entry.gameNumber;
+    printObj['included'] = entry.included ?  c.green(entry.included) : c.red(entry.included);
+    printObj['sets'] = entry.sets.map(printGameSet).join("   " );
+    const padLength = 50
+    printList.push(printObj);
+    
+    // console.log(`${i + 1} - gameNumber: ${entry.gameNumber.toString().padEnd(4, ' ')} - include: ${entry.included}- sets ${entry.sets.map(printGameSet).join('|').padEnd(padLength, ' ')} - original ${entry.line}`);
   }
+  console.table(printList);
+}
+
+
+
+
+
+const filterGamesOnPossibleSet = (result: ParsedResult, maxGameSet: GameSet): ParsedResult => {
+  
+  result.entries.forEach((entry) => {
+    entry.included = entry.sets.every(s => s.red <= maxGameSet.red && s.blue <= maxGameSet.blue && s.green <= maxGameSet.green);
+  });
+  return result;
+
 }
 
 
@@ -67,17 +131,34 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     const entry = parseLines(getExampleInput())
 
     expect(entry.entries[0].line).toEqual('Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green')
-    printResult(entry)
+    // printResult(entry)
 
   })
 
+
+  test('parse lines filter', () => {
+    const entry = parseLines(getExampleInput())
+
+    const filterGames = filterGamesOnPossibleSet(entry, {red: 12, blue: 13, green: 14})
+
+
+    const sum = filterGames.entries.filter(x => x.included ).reduce((acc, curr) => acc + curr.gameNumber, 0);
+    expect(sum).toEqual(8);
+
+    printResult(filterGames)
+
+  })
 
 
   test('answer', async () => {
     const testDataRaw = await readFileByLines(RAW_DATA_PATH)
 
     const entry = parseLines(testDataRaw)
-    expect(entry.entries[0].line.startsWith('Game 1: 1 green, 1 blue, 1 red; ')).toBeTruthy()
+
+    const filterGames = filterGamesOnPossibleSet(entry, {red: 12, blue: 13, green: 14})
+    printResult(filterGames)
+    const sum = filterGames.entries.filter(x => x.included ).reduce((acc, curr) => acc + curr.gameNumber, 0);
+    // expect(sum).toEqual(1234) // not yet right
   })
 
 
