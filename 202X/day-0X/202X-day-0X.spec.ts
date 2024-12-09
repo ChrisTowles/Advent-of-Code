bc6101093213fa8d6b61998562191769648f1818
Chris Towles <Chris.Towles@gmail.com>
2024-12-08 19:14:01 -0500

    chore: fix template

--- 202X/day-0X/202X-day-0X.spec.ts
+++ 202X/day-0X/202X-day-0X.spec.ts
@@ -5,7 +5,6 @@ import { readFileByLines, splitByNewLinesAndRemoveEmpty } from '../../utils/read
 // Constants
 const YEAR = "202X"
 const DAY = "0X"
-const PRINT_OUTPUT = false
 
 // Paths
 const RAW_DATA_PATH = `${YEAR}/day-${DAY}/${YEAR}-day-${DAY}.data.txt`
@@ -50,27 +49,7 @@ const solvePart2 = async(input: ParsedResult): Promise<Number> => {
   return 0;
 }
 
-
-const printResult = (result: ParsedResult) => {
-  const padLength = 15
-  const printList: any[] = [];
-
-  for (const entry of result.entries) {
-    const printObj = {}
-    printObj['line'] = entry.line.padEnd(padLength, ' ');
-    printList.push(printObj);
-  
-  }
-  
-  if(PRINT_OUTPUT) {
-    console.table(printList);
-  }
-}
-
-
 // -------------------- tests below this -------------------
-
-
 
 describe(TITLE, () => {
 
@@ -80,44 +59,31 @@ describe(TITLE, () => {
     `;
 
   test('example: part 1', async () => {
-
     const entry = parseLines(splitByNewLinesAndRemoveEmpty(exampleDataPart1))
-    printResult(entry)
     const answer = await solvePart1(entry)
     expect(answer).toEqual(0)
-
   })
-
-
 
   test('answer: part 1', async () => {
     const testDataRaw = await readFileByLines(RAW_DATA_PATH)
 
     const entry = parseLines(testDataRaw)
-    printResult(entry)
     const answer = await solvePart1(entry)
     expect(answer).toEqual(0)
 
   })
 
-
   test('example: part 2', async () => {
     const entry = parseLines(splitByNewLinesAndRemoveEmpty(exampleDataPart1))
-    printResult(entry)
     const answer = await solvePart2(entry)
     expect(answer).toEqual(0)
   })
-
-
 
   test('answer: part 2', async () => {
     const testDataRaw = await readFileByLines(RAW_DATA_PATH)
-
     const entry = parseLines(testDataRaw)
     const answer = await solvePart2(entry)
-    expect(answer).toEqual(0)
-   
+    expect(answer).toEqual(0) 
   })
-
 
 })
